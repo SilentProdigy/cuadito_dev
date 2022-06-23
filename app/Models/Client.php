63a52126e9afe510c464d630d507f697348c3a04
Brian Calma <briancalmadevacc@gmail.com>
2022-06-23 22:06:46 +0800

    setup client auth guard

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Client extends Model
+class Client extends Authenticatable
 {
     use HasFactory, SoftDeletes;
 
