2c7cc35b4fb765aeb576278a2021c1ea15bb9fe3
Brian Calma <briancalmadevacc@gmail.com>
2022-06-29 12:30:51 +0800

    created requirements table & model

---
+++ database/seeders/RequirementsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Requirement;
+use Illuminate\Database\Seeder;
+
+class RequirementsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $requirements = [
+            ['name' => "1pc Picture"],
+            ['name' => "1pc Valid ID(Borrower)"],
+            ['name' => "1pc Valid ID(Co-Maker)"],
+            ['name' => "Brgy. Business Permit"],
+        ];
+
+        foreach ($requirements as $item) 
+            Requirement::create($item);
+    }
+}
