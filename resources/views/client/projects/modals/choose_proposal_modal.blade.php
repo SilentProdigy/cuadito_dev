6dfde00cdefd159b72ed3e7783ceb721fd0b9227
Brian Calma <briancalmadevacc@gmail.com>
2022-07-20 12:59:36 +0800

    initial view for choose proposal modal

---
+++ resources/views/client/projects/modals/choose_proposal_modal.blade.php
@@ -0,0 +1,30 @@
+  <!-- Modal -->
+  <div class="modal fade" id="set-project-winner-modal" tabindex="-1" aria-labelledby="set-project-winner-modal" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title w-100 font-weight-bold" id="exampleModalLabel">Set Winning Proposal</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body mx-3 auth-form">
+                <form method="POST" action="#" id="set-project-status-form">
+                    @csrf
+                    @method('PATCH')
+                    <div class="row mb-3">
+                        <div class="col-md-12">
+                            <p>You are choosing <span id="company-name" class="text-danger fw-bold">Company</span> with the Proposal #ID of <span id="proposal-id" class="text-danger fw-bold">#00012</span>
+                               as the winner of this project.<br>Note that this action will mark the project's status to CLOSED. 
+                            </p>    
+
+                            <textarea name="note" rows="3" class="form-control" placeholder="Add some notes if neccessary"></textarea>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3 mx-0">
+                        <button type="submit" class="btn btn-outline-success">Continue</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
