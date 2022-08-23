b893f9605208710a43973b17048c32b5cdb25b97
Brian Calma <briancalmadevacc@gmail.com>
2022-08-23 22:37:08 +0800

    feat: added design for starred messages

--- resources/views/client/inbox/starred.blade.php
+++ resources/views/client/inbox/starred.blade.php
@@ -1,94 +1,28 @@
-@extends('layouts.client-main-layout')
+@extends('client.inbox.includes.inbox_layout')
 
-@section('content')    
+@section('main_room_section')    
 
-<div class="container-fluid mb-3">
-    <div class="d-flex flex-row d-align-items-center justify-content-center">
-        <div class="table-titles">Starred Emails</div>
-        <div class="col d-flex justify-content-end">
-            <button class="btn btn-warning header-btn" data-bs-toggle="modal" data-bs-target="#create-new-conversation-modal">
-                <i class="fa fa-plus"></i>&ensp;New Email
-            </button>
-        </div>
-    </div>
-
-    @include('client.inbox.includes.inbox_links')
-</div>
-
-@forelse ($conversation_subscriptions as $item)
-    <section class="border p-4 border-2 rounded my-2">
-        <div>
-            <div class="d-flex justify-content-between align-content-center">
-                <div class="fs-5 fw-bold">
-                    @if( $item->is_starred )
-                    <a href="#" class="text-warning btn-unstar" data-subscription="{{ json_encode($item) }}"><i class="fa fa-star fa-star"></i></a> 
-                    @endif 
-                    
-                    {{ $item->conversation->other_client->name }}
+<ul class="messages-list">
+    @foreach ($conversation_subscriptions as $item)
+        <li class="unread">
+            <a href="{{ route('client.conversations.show', $item->conversation) }}">
+                <div class="header">
+                    <span class="action"><i class="fa fa-square-o"></i><i class="fa fa-square"></i></span> 
+                    <span class="from">{{ $item->conversation->other_client->name }}</span>
+                    <span class="date"><span class="fa fa-paper-clip"></span> {{ $item->conversation->latest_message->created_at->format('M d, Y, g:i A') }}</span>
                 </div>
-                <p class="fs-5 fw-bold">{{ $item->conversation->latest_message->created_at->format('M d, Y') }}</p>
-            </div>
+                <div class="title">
+                    <span class="action"><i class="fa fa-star-o"></i><i class="fa fa-star bg"></i></span>
+                    {{ $item->conversation->subject }}
+                </div>	
+                <div class="description">
+                    {!! $item->conversation->latest_message->content_text !!}
+                </div>
+            </a>		
+        </li>
+    @endforeach
+</ul>
 
-            <p class="fs-6 fw-bold">{{ $item->conversation->subject }}</p>
-        </div>
-
-        <div>
-            <p class="fs-6 fw-light">
-                {!! $item->conversation->latest_message->content_text !!}
-            </p>	
-        </div>
-
-
-        <div class="d-flex justify-content-end align-content-center">
-            <a href="{{ route('client.conversations.show', $item->conversation) }}" class="text-info p-1">
-                <i class="fa fa-eye"></i>
-            </a>
-            <a href="javascript::void()" class="text-dark p-1 btn-archive" data-subscription="{{ json_encode($item) }}">
-                <i class="fa fa-archive"></i>
-            </a>
-            @if(!$item->is_starred)
-                <a href="#" class="text-warning p-1 btn-star" data-subscription="{{ json_encode($item) }}">
-                    <i class="fa fa-star"></i>
-                </a>
-            @endif
-            <a href="#" class="text-danger p-1 btn-delete" data-subscription="{{ json_encode($item) }}">
-                <i class="fa fa-trash"></i>
-            </a>
-        </div>
-    </section>
-
-    <form action="#" id="star-form" method="POST">
-        @csrf
-        <input type="hidden" name="star" value="true" id="star-txt">
-    </form>
-
-    <section class="mt-3 d-flex justify-content-center">
-        {{-- <nav aria-label="Page navigation example">
-            <ul class="pagination justify-content-center">
-                <li class="page-item disabled">
-                <a class="page-link">Previous</a>
-                </li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item">
-                <a class="page-link" href="#">Next</a>
-                </li>
-            </ul>
-        </nav> --}}
-            
-        {{ $conversation_subscriptions->links() }}
-    </section>
-@empty
-    <section class="border p-4 border-2 rounded my-2">
-        <p class="fs-4">You don't have any starred emails!</p>
-    </section>
-@endforelse
-
-
-@include('client.conversations.includes.create_conversation_modal')
-@include('client.conversations.includes.confirm_archived_modal')
-@include('client.conversations.includes.confirm_delete_modal')
 @endsection
 
 @section('script')
