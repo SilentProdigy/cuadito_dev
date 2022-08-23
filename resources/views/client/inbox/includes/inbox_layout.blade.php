db120d73f2973857c42a0094fbccb8261ff57f77
Brian Calma <briancalmadevacc@gmail.com>
2022-08-23 22:36:10 +0800

    feat: inbox layout

---
+++ resources/views/client/inbox/includes/inbox_layout.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.client-main-layout')
+
+@section('content')
+<div class="container px-5">
+    <div class="row inbox">
+        <div class="col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-body inbox-menu">
+                    <button class="btn btn-danger btn-block" data-bs-toggle="modal" data-bs-target="#create-new-conversation-modal">New Email</button>
+                    @include('client.inbox.includes.rooms_links')
+                    @include('client.inbox.includes.labels_links')
+                </div>	
+            </div>
+            @include('client.inbox.includes.contact_list')
+        </div><!--/.col-->
+        
+        <div class="col-md-9">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    
+                    @include('client.inbox.includes.email_actions')
+
+                    @yield('main_room_section')
+                </div>	
+                
+            </div>	
+
+            
+        </div><!--/.col-->	
+                
+    </div>
+</div>
+
+
+@include('client.includes.set_company_modal')
+@include('client.conversations.includes.create_conversation_modal')
+@include('client.conversations.includes.create_conversation_modal')
+@include('client.conversations.includes.confirm_archived_modal')
+@include('client.conversations.includes.confirm_delete_modal')
+@endsection
+
+@section('script')
+
+@endsection
