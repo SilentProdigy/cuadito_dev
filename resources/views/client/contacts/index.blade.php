a1d1858d3e1c5a4aa4be77a992650a41d3d95fca
gracefaithdelgado <anthony@cict.solutions>
2022-09-01 12:48:50 +0800

    feat: contact_page

--- resources/views/client/contacts/index.blade.php
+++ resources/views/client/contacts/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.client-main-layout')
 
-@section('content')    
+@section('content')
 
 <div class="container-fluid mb-3">
     <div class="d-flex flex-row d-align-items-center justify-content-center">
@@ -13,11 +13,159 @@
     </div>
 </div>
 
-<div class="card">
+<div class="container-fluid mb-3 d-flex flex-row">
+    <div class="bg-white">
+        <div class="row">
+            <div class="col-xl-6 mb-4">
+                <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center">
+                    <div class="d-flex align-items-center">
+                        <img
+                        src="https://mdbootstrap.com/img/new/avatars/8.jpg"
+                        alt=""
+                        style="width: 45px; height: 45px"
+                        class="rounded-circle"
+                        />
+                        <div class="ms-3">
+                        <p class="fw-bold mb-1">John Doe</p>
+                        <p class="text-muted mb-0">john.doe@gmail.com</p>
+                        </div>
+                    </div>
+                    <span class="badge rounded-pill badge-success">Active</span>
+                    </div>
+                </div>
+                <div
+                    class="card-footer border-0 bg-light p-2 d-flex justify-content-around"
+                >
+                    <a
+                    class="btn btn-link m-0 text-reset"
+                    href="#"
+                    role="button"
+                    data-ripple-color="primary"
+                    >Message<i class="fas fa-envelope ms-2"></i
+                    ></a>
+                    <a
+                    class="btn btn-link m-0 text-reset"
+                    href="#"
+                    role="button"
+                    data-ripple-color="primary"
+                    >Call<i class="fas fa-phone ms-2"></i
+                    ></a>
+                </div>
+                </div>
+            </div>
+            <div class="col-xl-6 mb-4">
+                <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center">
+                    <div class="d-flex align-items-center">
+                        <img
+                        src="https://mdbootstrap.com/img/new/avatars/6.jpg"
+                        alt=""
+                        style="width: 45px; height: 45px"
+                        class="rounded-circle"
+                        />
+                        <div class="ms-3">
+                        <p class="fw-bold mb-1">Alex Ray</p>
+                        <p class="text-muted mb-0">alex.ray@gmail.com</p>
+                        </div>
+                    </div>
+                    <<span class="badge rounded-pill badge-success">Active</span>
+                    </div>
+                </div>
+                <div
+                    class="card-footer border-0 bg-light p-2 d-flex justify-content-around"
+                >
+                    <a
+                    class="btn btn-link m-0 text-reset"
+                    href="#"
+                    role="button"
+                    data-ripple-color="primary"
+                    >Invite<i class="fas fa-paper-plane ms-2"></i
+                    ></a>
+                </div>
+                </div>
+            </div>
+            <div class="col-xl-6 mb-4">
+                <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center">
+                    <div class="d-flex align-items-center">
+                        <img
+                        src="https://mdbootstrap.com/img/new/avatars/7.jpg"
+                        alt=""
+                        style="width: 45px; height: 45px"
+                        class="rounded-circle"
+                        />
+                        <div class="ms-3">
+                        <p class="fw-bold mb-1">Kate Hunington</p>
+                        <p class="text-muted mb-0">kate.hunington@gmail.com</p>
+                        </div>
+                    </div>
+                    <span class="badge rounded-pill badge-danger">Away</span>
+                    </div>
+                </div>
+                <div
+                    class="card-footer border-0 bg-light p-2 d-flex justify-content-around"
+                >
+                <a
+                    class="btn btn-link m-0 text-reset"
+                    href="#"
+                    role="button"
+                    data-ripple-color="primary"
+                    >Invite<i class="fas fa-paper-plane ms-2"></i
+                    ></a>
+                </div>
+                </div>
+            </div>
+            <div class="col-xl-6 mb-4">
+                <div class="card">
+                <div class="card-body">
+                    <div class="d-flex justify-content-between align-items-center">
+                    <div class="d-flex align-items-center">
+                        <img
+                        src="https://mdbootstrap.com/img/new/avatars/3.jpg"
+                        alt=""
+                        style="width: 45px; height: 45px"
+                        class="rounded-circle"
+                        />
+                        <div class="ms-3">
+                        <p class="fw-bold mb-1">Michael Bale</p>
+                        <p class="text-muted mb-0">michael.bale@gmail.com</p>
+                        </div>
+                    </div>
+                    <span class="badge rounded-pill badge-danger">Away</span>
+                    </div>
+                </div>
+                <div
+                    class="card-footer border-0 bg-light p-2 d-flex justify-content-around"
+                >
+                    <a
+                    class="btn btn-link m-0 text-reset"
+                    href="#"
+                    role="button"
+                    data-ripple-color="primary"
+                    >Message<i class="fas fa-envelope ms-2"></i
+                    ></a>
+                    <a
+                    class="btn btn-link m-0 text-reset"
+                    href="#"
+                    role="button"
+                    data-ripple-color="primary"
+                    >Call<i class="fas fa-phone ms-2"></i
+                    ></a>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- <div class="card">
     <div class="card-body">
         <table class="table table-borderless table-md user-listing-table">
             <thead>
-                {{-- <th>SEQ</th> --}}
                 <th>NAME</th>
                 <th>EMAIL ADDRESS</th>
                 <th>ACTIONS</th>
@@ -48,7 +196,7 @@
             </tbody>
         </table>
     </div>
-</div>
+</div> -->
 
 @endsection
 
