1cb2963dee3e0a4ffe6341d44ffe661e0eca021b
Brian Calma <briancalmadevacc@gmail.com>
2022-09-05 17:02:38 +0800

    feat: contact list on the inbox sidebar

--- resources/views/client/inbox/includes/contact_list.blade.php
+++ resources/views/client/inbox/includes/contact_list.blade.php
@@ -1,27 +1,16 @@
 <div class="panel panel-default">       
     <div class="panel-body contacts">
         
-        <a href="#" class="btn btn-success btn-block"> + Contact</a>
+        <a href="{{ route('client.contacts.create') }}" class="btn btn-success btn-block"> + Contact</a>
 
         <ul>
-            <li><span class="label label-danger"></span> Adam Alister</li>
-            <li><span class="label label-default"></span> Alphonse Ivo</li>
-            <li><span class="label label-success"></span> Anton Phunihel</li>
-            <li><span class="label label-success"></span> Ajith Hristijan</li>
-            <li><span class="label label-warning"></span> Bao Gaspar</li>
-            <li><span class="label label-default"></span> Bernhard Shelah</li>
-            <li><span class="label label-success"></span> Bünyamin Kasper</li>
-            <li><span class="label label-danger"></span> Carlito Roffe</li>
-            <li><span class="label label-danger"></span> Chidubem Gottlob</li>
-            <li><span class="label label-warning"></span> Dederick Mihail</li>
-            <li><span class="label label-success"></span> Felice Arseniy</li>
-            <li><span class="label label-default"></span> Grahame Miodrag</li>
-            <li><span class="label label-default"></span> Hristofor Sergio</li>
-            <li><span class="label label-danger"></span> Scottie Maximilian</li>
-            <li><span class="label label-danger"></span> Sullivan Robert</li>
-            <li><span class="label label-danger"></span> Thancmar Theophanes</li>
-            <li><span class="label label-warning"></span> Tullio Luka</li>
-            <li><span class="label label-success"></span> Walerian Khwaja</li>
+            @foreach($contacts as $item)
+                <li>
+                    <a href="{{ route('client.conversations.create', ['email' => $item->contact_email]) }}">
+                        <span class="label label-danger"></span> {{ $item->contact_name }}
+                    </a>
+                </li>
+            @endforeach
         </ul>
     
     </div>
