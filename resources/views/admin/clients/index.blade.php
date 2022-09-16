44b8eeca15ccde4dc0a94853134fee5c62b844fe
Brian Calma <briancalmadevacc@gmail.com>
2022-09-16 13:11:27 +0800

    added additional columns

--- resources/views/admin/clients/index.blade.php
+++ resources/views/admin/clients/index.blade.php
@@ -3,8 +3,17 @@
 @section('content')
 
     <div class="container-fluid mb-3">
-        <div class="d-flex flex-row d-align-items-center">
+        <div class="d-flex flex-row justify-content-between align-items-center">
             <div class="table-titles">Clients</div>
+            <form action="{{ route('admin.clients.index') }}" method="get" class="d-flex justify-content-between align-items-center">
+                <div class="input-group input-group-lg">
+                    <input type="text" class="form-control " placeholder="Search Client ..." name="search" value="{{ request('search') }}">
+                    <button class="btn btn-warning" type="submit">
+                        <span class="fa fa-search"></span>
+                    </button>
+                </div>
+                <a href="{{ route('admin.clients.index') }}" class="p-2">Clear</a>
+            </form>
         </div>
     </div>
     <div class="card">
@@ -14,7 +23,10 @@
                     <th>SEQ</th>
                     <th>NAME</th>
                     <th>EMAIL</th>
+                    <th>CONTACT NO.</th>
                     <th>COMPANIES</th>
+                    <th>PROJECTS</th>
+                    <th>BIDDINGS</th>
                     <th>Actions</th>
                 </thead>
                 <tbody>
@@ -30,7 +42,16 @@
                                 <span>{{ $client->email }}</span>
                             </td>
                             <td>
+                                <span>{{ $client->contact_number }}</span>
+                            </td>
+                            <td>
                                 <span>{{ $client->companies_count }}</span>
+                            </td>
+                            <td>
+                                <span>{{ $client->projects_count }}</span>
+                            </td>
+                            <td>
+                                <span>{{ $client->biddings_count }}</span>
                             </td>
                             <td class="user-actions">
                                 <a href="{{ route('admin.clients.show', $client) }}" class="btn btn-sm btn-outline-info">
