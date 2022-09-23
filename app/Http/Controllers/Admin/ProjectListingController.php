d42eee077929e483544627edc3222dd096836190
Brian Calma <briancalmadevacc@gmail.com>
2022-09-23 13:37:24 +0800

    project index and set status

--- app/Http/Controllers/Admin/ProjectListingController.php
+++ app/Http/Controllers/Admin/ProjectListingController.php
@@ -3,12 +3,32 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Client\Project\UpdateProjectStatusRequest;
+use App\Models\Project;
+use Exception;
 use Illuminate\Http\Request;
 
 class ProjectListingController extends Controller
 {
     public function index()
     {
-        return 'project listing';
+        $projects = Project::get();
+        $project_states = Project::PROJECT_STATES;
+
+        return view('admin.projects.index')->with(compact('projects', 'project_states'));
+    }
+
+    public function setStatus(UpdateProjectStatusRequest $request, Project $project)
+    {
+        try 
+        {
+            // TODO: Additional business logic here ...
+            $project->update($request->all());
+            return redirect(route('admin.projects.index'))->with('success', 'Project status was successfully set.');  
+        }
+        catch(Exception $e)
+        {
+            dd($e->getMessage());
+        }
     }
 }
