d9f74ceeea3a6edc3cb87e6c86d44e24eef62cbb
Brian Calma <briancalmadevacc@gmail.com>
2022-10-13 13:07:54 +0800

    added ItemNotFoundException catcher for Requirement Delete

--- app/Http/Controllers/Client/CompanyRequirementController.php
+++ app/Http/Controllers/Client/CompanyRequirementController.php
@@ -10,6 +10,7 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use App\Traits\UploadFile;
+use Illuminate\Support\Facades\DB;
 
 class CompanyRequirementController extends Controller
 {
@@ -78,13 +79,35 @@ class CompanyRequirementController extends Controller
 
     public function destroy(Request $request, Company $company, Requirement $requirement)
     {    
-        $old_file = $company->requirements->where('id', $requirement->id)->first();
+        DB::transaction();
 
-        $this->deleteFile($old_file->file->url);
+        try
+        {
+            $old_file = $company->requirements->where('id', $requirement->id)->firstOrFail();
+    
+            $this->deleteFile($old_file->file->url);
+    
+            $company->requirements()->detach($requirement->id);
 
-        $company->requirements()->detach($requirement->id);
+            DB::commit();
+    
+            return redirect()->back()->with('success', 'Requirement was successfully deleted.');  
+        }
+        catch(\Illuminate\Support\ItemNotFoundException $e)
+        {
+            return redirect(route('client.companies.show', $company))->withErrors([
+                "Company requirement not found!"
+            ]);
+        }
+        catch(\Exception $e)
+        {
+            DB::rollBack();
 
-        return redirect()->back()->with('success', 'Requirement was successfully deleted.');  
+            return redirect(route('client.companies.show', $company))
+                    ->withErrors([
+                        'Operation Failed!' => $e->getMessage()
+                    ]);
+        }
     }
 
 }
