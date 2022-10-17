09af0857b6428aea56f45202c848533acabdc7e2
Brian Calma <briancalmadevacc@gmail.com>
2022-10-17 13:07:58 +0800

    used @json()

--- resources/views/client/contacts/includes/contact_list.blade.php
+++ resources/views/client/contacts/includes/contact_list.blade.php
@@ -1,48 +1,49 @@
 @forelse ($contacts as $item)
-<div class="col-xl-6 mb-4">
-    <div class="card">
-        <div class="card-body">
-            <div class="d-flex justify-content-between align-items-center">
-                <div class="d-flex align-items-center">
-                    <img
-                    src="https://mdbootstrap.com/img/new/avatars/8.jpg"
-                    alt=""
-                    style="width: 45px; height: 45px"
-                    class="rounded-circle"
-                    />
-                    <div class="ms-3">
-                    <p class="fw-bold mb-1">{{ $item->contact_name }}</p>
-                    <p class="text-muted mb-0">{{ $item->contact_email }}</p>
+    <div class="col-xl-6 mb-4">
+        <div class="card">
+            <div class="card-body">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="d-flex align-items-center">
+                        <img
+                        src="https://mdbootstrap.com/img/new/avatars/8.jpg"
+                        alt=""
+                        style="width: 45px; height: 45px"
+                        class="rounded-circle"
+                        />
+                        <div class="ms-3">
+                            <p class="fw-bold mb-1">{{ $item->contact_name }}</p>
+                            <p class="text-muted mb-0">{{ $item->contact_email }}</p>
+                        </div>
                     </div>
+                    <span class="badge rounded-pill badge-success">Active</span>
                 </div>
-                <span class="badge rounded-pill badge-success">Active</span>
+            </div>
+            <div class="card-footer border-0 bg-light p-2 d-flex justify-content-around">
+                @if($item->is_existing_client)
+                    <a
+                        class="btn btn-link m-0 text-reset"
+                        href="{{ route('client.conversations.create', ['email' => $item->contact_email]) }}"
+                        role="button"
+                        data-ripple-color="primary"
+                    >Message<i class="fas fa-envelope ms-2"></i
+                    ></a>
+                    <a class="btn btn-link m-0 text-reset btn-delete-contact"
+                        href="#"
+                        role="button"
+                        data-ripple-color="primary"
+                        data-contact='@json($item)'>Delete<i class="fas fa-trash ms-2"></i>
+                    </a>
+                @else 
+                    <a class="btn btn-link m-0 text-reset" 
+                        href="{{ route('client.contacts.invite', $item) }}" 
+                        role="button" 
+                        data-ripple-color="primary">
+                        Invite<i class="fas fa-paper-plane ms-2"></i>
+                    </a>
+                @endif
             </div>
         </div>
-        <div class="card-footer border-0 bg-light p-2 d-flex justify-content-around">
-            @if($item->is_existing_client)
-                <a
-                    class="btn btn-link m-0 text-reset"
-                    href="{{ route('client.conversations.create', ['email' => $item->contact_email]) }}"
-                    role="button"
-                    data-ripple-color="primary"
-                >Message<i class="fas fa-envelope ms-2"></i
-                ></a>
-                <a 
-                    class="btn btn-link m-0 text-reset btn-delete-contact"
-                    href="#"
-                    role="button"
-                    data-ripple-color="primary"
-                    data-contact="{{ json_encode($item) }}"
-                >Delete<i class="fas fa-trash ms-2"></i
-                ></a>
-            @else 
-                <a class="btn btn-link m-0 text-reset" href="{{ route('client.contacts.invite', $item) }}" role="button" data-ripple-color="primary">
-                    Invite<i class="fas fa-paper-plane ms-2"></i>
-                </a>
-            @endif
-        </div>
     </div>
-</div>
 @empty
-<p>You don't have any contacts yet!</p>
+    <p>You don't have any contacts yet!</p>
 @endforelse
--- resources/views/client/contacts/index.blade.php
+++ resources/views/client/contacts/index.blade.php
@@ -1,51 +1,50 @@
 @extends('layouts.client-main-layout')
 
 @section('content')
-
-<div class="container-fluid mb-3">
-    <div class="d-flex flex-row d-align-items-center justify-content-center">
-        <div class="table-titles">Your Contacts</div>
-        <div class="col d-flex gap-4 flex-row-reverse">
-            <a href="{{ route('client.contacts.create') }}" class="btn btn-orange header-btn">
-                <i class="fa fa-plus"></i>&ensp;Add Contact
-            </a>
-            <div>
-                <form action="{{ route('client.contacts.index') }}" method="GET">
-                    <div class="input-group">
-                        <div class="form-outline">
-                            <input id="search-focus" type="search" id="form1" class="form-control" name="search"/>
-                            <label class="form-label" for="form1">Search</label>
+    <div class="container-fluid mb-3">
+        <div class="d-flex flex-row d-align-items-center justify-content-center">
+            <div class="table-titles">Your Contacts</div>
+            <div class="col d-flex gap-4 flex-row-reverse">
+                <a href="{{ route('client.contacts.create') }}" class="btn btn-orange header-btn">
+                    <i class="fa fa-plus"></i>&ensp;Add Contact
+                </a>
+                <div>
+                    <form action="{{ route('client.contacts.index') }}" method="GET">
+                        <div class="input-group">
+                            <div class="form-outline">
+                                <input id="search-focus" type="search" id="form1" class="form-control" name="search"/>
+                                <label class="form-label" for="form1">Search</label>
+                            </div>
+                            <button type="submit" class="btn border-orange btn-orange">
+                                <i class="fas fa-search"></i>
+                            </button>
                         </div>
-                        <button type="submit" class="btn border-orange btn-orange">
-                            <i class="fas fa-search"></i>
-                        </button>
-                    </div>
-                </form>
-            </div> 
+                    </form>
+                </div> 
+            </div>
         </div>
     </div>
-</div>
 
-<div class="container-fluid mb-3 d-flex flex-row">
-    <div class="bg-white">
-        <div class="row">
-            @includeWhen(!request()->has('search'), 'client.contacts.includes.contact_list')
-            @includeWhen(request()->has('search'), 'client.contacts.includes.client_list')
+    <div class="container-fluid mb-3 d-flex flex-row">
+        <div class="bg-white">
+            <div class="row">
+                @includeWhen(!request()->has('search'), 'client.contacts.includes.contact_list')
+                @includeWhen(request()->has('search'), 'client.contacts.includes.client_list')
+            </div>
         </div>
     </div>
-</div>
 
-<form action="{{ route('client.connect.store')}}" method="post" id="connect-form">
-    @csrf
-    <input type="hidden" name="contact_id" id="contact-id-txt">
-</form>
+    <form action="{{ route('client.connect.store')}}" method="post" id="connect-form">
+        @csrf
+        <input type="hidden" name="contact_id" id="contact-id-txt">
+    </form>
 
-<form action="#" method="post" id="delete-connect-form">
-    @csrf
-    @method('DELETE')
-    <input type="hidden" name="contact_id" id="delete-contact-id-txt">
-    <input type="hidden" name="type" id="delete-contact-type-txt">
-</form>
+    <form action="#" method="post" id="delete-connect-form">
+        @csrf
+        @method('DELETE')
+        <input type="hidden" name="contact_id" id="delete-contact-id-txt">
+        <input type="hidden" name="type" id="delete-contact-type-txt">
+    </form>
 @endsection
 
 @section('script')
@@ -78,9 +77,8 @@
                     e.preventDefault;
                     let data = button.getAttribute('data-contact');   
                     data = JSON.parse(data);
-                    
+            
                     let form = document.querySelector('#delete-connect-form');
-                    // form.setAttribute('action', `/client/connect`);
                     $('#delete-contact-id-txt').val(`${ data.id }`);
                     $('#delete-contact-type-txt').val('contact');
 
