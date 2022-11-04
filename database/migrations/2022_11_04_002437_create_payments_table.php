c2dda8ca865378a7868b1dafaadbc04c338e974b
Brian Calma <briancalmadevacc@gmail.com>
2022-11-04 08:53:00 +0800

    subscription related migrations

---
+++ database/migrations/2022_11_04_002437_create_payments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('subscription_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->decimal('amount', 9, 2)->nullable();
+            $table->decimal('additional_vat', 9, 2)->nullable();
+            $table->decimal('total_amount', 9, 2)->nullable();
+            $table->string('mode_of_payment')->nullable();
+            $table->text('details')->nullable();
+            $table->timestamps();
+
+            $table->index(['subscription_id','mode_of_payment']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
