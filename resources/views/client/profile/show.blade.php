6860ca5f67f6703627a2515ef9f701f39132b522
anthonyAIA <anthony@australianinternetadvertising.com.au>
2023-01-03 15:13:32 +0800

    Profile Page Changes

--- resources/views/client/profile/show.blade.php
+++ resources/views/client/profile/show.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.client-main-layout')
 
+@section('style')
+<style>
+    .profile_info .btn{
+        background-color: rgba(211, 211, 211, 0.5);
+        box-shadow: none;
+    }
+    .profile_info .btn:hover{
+        background-color: gray;
+        color: #fff;
+    }
+    .profile_details i{
+        margin-right: 10px;
+    }
+</style>
+@endsection
+
 @section('content')
 <div class="container-fluid user-profile">
     <div class="card profile-header">
@@ -14,83 +30,59 @@
                     <span>{{ $client->email }}</span>
                 </div>
                 <div class="col-md-6">
-                    <a href="{{ route('client.profile.change-password.form', $client) }}" class="btn btn-sm btn-warning float-end">Change Password</a>
-                    <a href="{{ route('client.profile.edit', auth('client')->user()) }}" class="btn btn-sm btn-info float-end mx-3">Edit Account Info</a>
+                    <a href="{{ route('client.profile.change-password.form', $client) }}" class="btn btn-sm btn-warning float-end shadow-none"><i class="fa fa-lock"></i>&ensp;Change Password</a>
                 </div>
             </div>
         </div>
     </div>
-
-    <div class="row mt-5 my-5">
-        <div class="col-md-8">
-            <h5>About</h5>
-            <div class="my-1 py-1">
-                <h5 class="text-uppercase text-secondary fw-bold fs-6 py-1">CONTACT NUMBER</h5>
-                <p class="fs-6 lh-lg" style="color: #222;">{{ $client->contact_number }}</p>
-            </div>
-
-            <div class="my-1 py-1">
-                <h5 class="text-uppercase text-secondary fw-bold fs-6 py-1">GENDER</h5>
-                <p class="fs-6 lh-lg" style="color: #222;">{{ $client->gender }}</p>
-            </div>
-
-            <div class="my-1 py-1">
-                <h5 class="text-uppercase text-secondary fw-bold fs-6 py-1">BIRTHDATE</h5>
-                <p class="fs-6 lh-lg" style="color: #222;">{{ $client->birth_date }}</p>
-            </div>
-
-            <div class="my-1 py-1">
-                <h5 class="text-uppercase text-secondary fw-bold fs-6 py-1">MARITAL STATUS</h5>
-                <p class="fs-6 lh-lg" style="color: #222;">{{ $client->marital_status }}</p>
-            </div>
-
-            <div class="my-1 py-1">
-                <h5 class="text-uppercase text-secondary fw-bold fs-6 py-1">ADDRESS</h5>
-                <p class="fs-6 lh-lg" style="color: #222;">{{ $client->address }}</p>
-            </div>
-        </div>
-        <div class="col-md-4">
-            @if($client->have_subscription)
-                <div class="mt-3 subscriptions">
-                    <div class="d-flex justify-content-between align-items-center">
-                        <h4>Subscription Plan</h4>
-                        <!-- <a href="{{ route('client.products.index') }}" class="btn btn-sm btn-orange">{{ $client->active_subscription->subscription_type->name }}</a>
-                         -->
-                        <button type="button" class="btn btn-sm btn-orange" data-bs-toggle="modal" data-bs-target="#subscriptionModal">
-                            {{ $client->active_subscription->subscription_type->name }}
-                        </button>
-                    </div>
-                    <div>
-                        <div class="my-1 py-1">
-                            <p class="counters" style="color: #222;">{{ $client->active_subscription->remaining_projects }}</p>
-                            <h5 class="text-uppercase text-secondary fw-bold">REMAINING PROJECT</h5>
+    <div class="mx-auto mt-5 my-5">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="card profile_info">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <h4 class="fw-bold">About</h4>
+                            <a href="{{ route('client.profile.edit', auth('client')->user()) }}" class="btn btn-md"><i class="fa fa-pencil"></i>&ensp;Edit Profile</a>
                         </div>
-                        <div class="my-1 py-1">
-                            <p class="counters" style="color: #222;">{{ $client->active_subscription->remaining_proposals }}</p>
-                            <h5 class="text-uppercase text-secondary fw-bold">REMAINING PROPOSALS</h5>
+                        <div class="profile_details row mt-4">
+                            <div class="col-12 mt-4"><i class="fa fa-briefcase fs-5"></i>Website Developer | 1MC Digital</div>
+                            <div class="col-12 mt-4"><i class="fa fa-phone-alt fs-5"></i>{{ $client->contact_number }}</div>
+                            <div class="col-12 mt-4"><i class="fa fa-location-dot fs-5"></i>{{ $client->address }}</div>
+                            <div class="col-12 mt-4"><i class="fa fa-cake-candles fs-5"></i>{{ $client->birth_date }}</div>
+                            <div class="col-12 mt-4"><i class="fa fa-heart fs-5"></i>{{ $client->marital_status }}</div>
                         </div>
                     </div>
                 </div>
-            @endif
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        @if($client->have_subscription)
+                            <div class="mt-3 subscriptions">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <h4 class="fw-bold">Subscription Plan</h4>
+                                    <!-- <a href="{{ route('client.products.index') }}" class="btn btn-sm btn-orange">{{ $client->active_subscription->subscription_type->name }}</a>
+                                    -->
+                                    <button type="button" class="btn btn-sm btn-warning shadow-none" data-bs-toggle="modal" data-bs-target="#subscriptionModal">
+                                        {{ $client->active_subscription->subscription_type->name }}
+                                    </button>
+                                </div>
+                                <div>
+                                    <div class="my-1 py-1">
+                                        <p class="counters fw-bold">{{ $client->active_subscription->remaining_projects }}</p>
+                                        <h5 class="text-uppercase text-muted">Remaining Projects</h5>
+                                    </div>
+                                    <div class="my-1 py-1">
+                                        <p class="counters fw-bold">{{ $client->active_subscription->remaining_proposals }}</p>
+                                        <h5 class="text-uppercase text-muted">Remaining Proposals</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-    
-    {{-- <div class="card mt-3">
-        <div class="card-header">
-            <h5>Product / Services</h5>
-        </div>
-        <div class="card-body">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-    </div>
-    <div class="card mt-3">
-        <div class="card-header">
-            <h5>Projects</h5>
-        </div>
-        <div class="card-body">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </div>
-    </div> --}}
-
 </div>
 @endsection
