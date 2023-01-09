c1057fff72c2e5475972526c42e2f4915060c603
Brian Calma <briancalmadevacc@gmail.com>
2023-01-09 21:51:59 +0800

    fix: added placeholder values

--- resources/views/client/companies/show.blade.php
+++ resources/views/client/companies/show.blade.php
@@ -30,7 +30,7 @@
                                 <i class="fa fa-plus"></i>&ensp;Add Requirement
                             </button>
                         @endif
-                    </div>
+                        </div>
                     <div class="col">
                         @include('client.companies.includes.requirements_table')
                     </div>
@@ -38,7 +38,6 @@
                 <div class="col-md-12">
                     <h4 class="mt-3">ABOUT <span class="text-uppercase">{{ $company->name }}</span></h4>
                     <div class="company_description">
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur</p>
                         {{ $company->description }}
                     </div>
     
@@ -51,9 +50,8 @@
                 <div class="col-md-12">
                     <h4 class="mt-3">ABOUT <span class="text-uppercase">{{ $company->name }}</span></h4>
                     <div class="company_description">
-                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur</p>
                         @if($company->description)
-                            
+                            <p>{{ $company->description }}</p>
                         @else
                             <span>Please edit your company and add company description.</span>
                         @endif
