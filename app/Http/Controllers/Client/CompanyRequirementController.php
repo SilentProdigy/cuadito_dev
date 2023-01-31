8e8dd6cf37f98954588867968de0d9f677f2edee
Brian Calma <briancalmadevacc@gmail.com>
2023-01-31 21:08:48 +0800

    added xss-filter to upload company requirement

--- app/Http/Controllers/Client/CompanyRequirementController.php
+++ app/Http/Controllers/Client/CompanyRequirementController.php
@@ -11,27 +11,36 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use App\Traits\UploadFile;
 use Illuminate\Support\Facades\DB;
+use ProtoneMedia\LaravelXssProtection\Middleware\XssCleanInput;
 
 class CompanyRequirementController extends Controller
 {
     use UploadFile;
 
+    public function __construct()
+    {
+        $this->middleware(XssCleanInput::class)->only(['store']);
+    }
+
     public function store(UploadRequirementRequest $request, Company $company)
     {
-        try 
-        {
+        DB::beginTransaction();
+
+        try {
             $target_dir = 'companies/requirements/' . $company->id;
             $path = $this->uploadFile($target_dir, $request->file('requirement'));
-            
+
             $company->requirements()->attach(
                 $request->input('requirement_id'),
                 ['url' => $path]
             );
 
-            return redirect()->back()->with('success', 'Requirement was successfully uploaded.');     
-        }
-        catch(\Exception $e)
-        {
+            DB::commit();
+            return redirect()->back()->with('success', 'Requirement was successfully uploaded.');
+        } catch (\Exception $e) {
+
+            DB::rollBack();
+
             return redirect(route('client.companies.show', $company))->withErrors([
                 'Operation Failed!' => $e->getMessage()
             ]);
@@ -41,73 +50,58 @@ class CompanyRequirementController extends Controller
 
     private function deleteFile($url)
     {
-        try 
-        {   
-            if( Storage::disk('public')->exists($url) )
-            {
+        try {
+            if (Storage::disk('public')->exists($url)) {
                 Storage::disk('public')->delete($url);
             }
-        }
-        catch(\Exception $e)
-        {
+        } catch (\Exception $e) {
             return redirect()->back()->with('errors', $e->getMessage());
         }
     }
 
     public function download(Company $company, Requirement $requirement)
     {
-        try 
-        {
+        try {
             $target_file = $company->requirements->where('id', $requirement->id)->firstOrFail();
 
             return Storage::disk('public')->download($target_file->file->url);
-        }
-        catch(\Illuminate\Support\ItemNotFoundException $e)
-        {
+        } catch (\Illuminate\Support\ItemNotFoundException $e) {
             return redirect(route('client.companies.show', $company))->withErrors([
                 "Company requirement not found!"
             ]);
-        }
-        catch(\Exception $e)
-        {
+        } catch (\Exception $e) {
             return redirect(route('client.companies.show', $company))
-                    ->withErrors([
-                        'Operation Failed!' => $e->getMessage()
-                    ]);
-        }
-    }   
-
-    public function destroy(Request $request, Company $company, Requirement $requirement)
-    {    
-        DB::beginTransaction();
-
-        try
-        {
-            $old_file = $company->requirements->where('id', $requirement->id)->firstOrFail();
-    
-            $this->deleteFile($old_file->file->url);
-    
-            $company->requirements()->detach($requirement->id);
-
-            DB::commit();
-    
-            return redirect()->back()->with('success', 'Requirement was successfully deleted.');  
-        }
-        catch(\Illuminate\Support\ItemNotFoundException $e)
-        {
-            return redirect(route('client.companies.show', $company))->withErrors([
-                "Company requirement not found!"
-            ]);
-        }
-        catch(\Exception $e)
-        {
-            DB::rollBack();
-
-            return redirect(route('client.companies.show', $company))
-                    ->withErrors([
-                        'Operation Failed!' => $e->getMessage()
-                    ]);
+                ->withErrors([
+                    'Operation Failed!' => $e->getMessage()
+                ]);
         }
     }
 
+    public function destroy(Request $request, Company $company, Requirement $requirement)
+    {
+        DB::beginTransaction();
+
+        try {
+            $old_file = $company->requirements->where('id', $requirement->id)->firstOrFail();
+
+            $this->deleteFile($old_file->file->url);
+
+            $company->requirements()->detach($requirement->id);
+
+            DB::commit();
+
+            return redirect()->back()->with('success', 'Requirement was successfully deleted.');
+        } catch (\Illuminate\Support\ItemNotFoundException $e) {
+            return redirect(route('client.companies.show', $company))->withErrors([
+                "Company requirement not found!"
+            ]);
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            return redirect(route('client.companies.show', $company))
+                ->withErrors([
+                    'Operation Failed!' => $e->getMessage()
+                ]);
+        }
+    }
 }
