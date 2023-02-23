c366d62a35c7a30832e40ae8a391c78bae691f62
Brian <briancalmadevacc@gmail.com>
2023-02-23 18:39:56 +0800

    feat: added search on projects

--- app/Http/Controllers/Client/ProjectController.php
+++ app/Http/Controllers/Client/ProjectController.php
@@ -51,7 +51,47 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        $projects = auth('client')->user()->projects ?? [];
+        $projects = auth('client')->user()->projects();
+
+        // return request()->all();
+        
+        if(request()->has('search'))
+        {
+            $projects = $projects->when(request('search'), function($query) {
+                $query->where('title', 'LIKE',  '%'. request('search') . '%');
+            });
+        }
+        elseif(request()->has('adv_search'))
+        {
+            $projects = $projects
+                        ->where(request('filter_col'), 'LIKE', '%'.request('filter_val') . '%')
+                        ->orderBy(request()->input('sort_col'), request()->input('sort_val'));
+        }
+
+        $projects = $projects->with('categories')
+                    ->where('status', Project::ACTIVE_STATUS);
+        
+        if(!request()->has('sort_col') && request()->input('sort_col') != "")
+        {
+            $projects = $projects->orderBy('created_at', 'desc');
+        }
+
+        $projects = $projects->paginate(5);
+
+        $search_options = [
+            'filter_cols' => [
+                ['label' => 'Title', 'value' => 'title'],
+                ['label' => 'Scope of Work','value' => 'scope_of_work'], 
+                ['label' => 'Cost', 'value' => 'cost']
+            ], 
+            'sort_by_cols' => [
+                ['label' => 'Title', 'value' => 'title'],
+                ['label' => 'Scope of Work','value' => 'scope_of_work'], 
+                ['label' => 'Cost', 'value' => 'cost'],
+                ['label' => 'Date Posted', 'value' => 'created_at']
+            ]
+        ];
+
         $project_states = Project::PROJECT_STATES;
         return view('client.projects.index')->with(compact('projects', 'project_states'));
     }
