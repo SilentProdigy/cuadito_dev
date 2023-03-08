a8efde1bf4d406481ea34cf8b62a50962155f8a4
Brian <briancalmadevacc@gmail.com>
2023-03-08 17:33:33 +0800

    fix: hide product modal

--- resources/views/layouts/client-main-layout.blade.php
+++ resources/views/layouts/client-main-layout.blade.php
@@ -283,7 +283,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     @yield('script')
 
-    @if(!request()->routeIs('client.payments.result'))
+    {{-- @if(!request()->routeIs('client.payments.result'))
         <script>
             $(document).ready(function(){
                 
@@ -294,7 +294,7 @@
                 }
             });
       </script>
-    @endif
+    @endif --}}
     <!-- MDB -->
     <script
     type="text/javascript"
